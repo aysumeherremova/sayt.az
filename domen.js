689ae6c53122759e2750eeedfb773e5d3e628859
aysumeherremova <aysumeheremovaa@gmail.com>
2025-03-23 11:54:33 +0400

    seifelerArasiKeçid

---
+++ domen.js
@@ -0,0 +1,13 @@
+document.getElementById("domainSearchForm").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    let domain = document.getElementById("domainSDL").value.trim();
+    let domainExtension = document.getElementById("domainTDL").value;
+    
+    if (domain === "") {
+        document.getElementById("errorText").style.visibility = "visible";
+        document.getElementById("errorText").innerText = "Domen adı daxil edin!";
+    } else {
+        document.getElementById("errorText").style.visibility = "hidden";
+        alert("Domain: " + domain + domainExtension);  
+    }
+});
